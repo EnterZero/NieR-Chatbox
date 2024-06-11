12bec56ea6712155a0a63135130e82e670e5fffb
EnterZero <c1x.chaz1q@gmail.com>
2024-06-11 11:53:05 +0200

    release

--- nieR.js
+++ nieR.js
@@ -1,10 +1,3 @@
-// Please use event listeners to run functions.
-document.addEventListener('onLoad', function (obj) {
-    // obj will be empty for chat widget
-    // this will fire only once when the widget loads
-});
-
-document.addEventListener('onEventReceived', function (obj) {
-    // obj will contain information about the event
-
-});
+// release
+document.addEventListener('onLoad', function (obj) { });
+document.addEventListener('onEventReceived', function (obj) { });
